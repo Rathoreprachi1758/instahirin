4e283552ca39f80af465329409d24fe1290be93b
Azeem <fzaidi@cloud-1.co>
2023-07-22 18:40:20 +0500

    mobile app

---
+++ resources/views/templates/mobile-ios-developer.blade.php
@@ -0,0 +1,187 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>iOS APP DEVELOPERS</h1>
+                                    <h2 class="">
+                                        <strong>CREATE</strong> APPLICATIONS <strong>FOR MOBILE DEVICES POWERED BY</strong> APPLE'S iOS <strong>OPERATING SYSTEM</strong>.
+                                    </h2>
+
+                                        <p class="p_color text-justify">iOS app developers specialize in building applications specifically for Apple's iOS platform, which powers devices like iPhones, iPads, and iPods touch. They possess expertise in programming languages like Swift or Objective-C and deeply understand the iOS SDK (Software Development Kit) and related tools.</p>
+                                           <br>
+                                        <a href="/get-a-free-consultation" class="btn_default mt-2">Hire iOS App Developer</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/iosApp_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init " data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>iOS app    <strong>developers are employed for various purposes and use cases, including:</strong></h2>
+                            
+                             
+                              
+                              
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon1.png" alt="#" /></span>
+                                <h6>CONSUMER MOBILE APPS</h6>
+                                <p>iOS app developers are hired to create mobile applications for end-users, catering to various industries such as social media, e-commerce, entertainment, travel, finance, health and fitness, and more. These apps are typically available on the Apple App Store for iOS device users.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon2.png" alt="#" /></span>
+                                <h6>ENTERPRISE MOBILE APPS</h6>
+                                <p>iOS app developers are crucial in developing custom applications for businesses and organizations. These apps serve internal purposes like employee management, sales tracking, inventory management, task management, and other business processes. Enterprise apps may also integrate with backend systems, databases, and other software solutions.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon3.png" alt="#" /></span>
+                                <h6>GAMING APPS</h6>
+                                <p>iOS app developers with game development expertise are responsible for creating interactive and engaging games specifically for iOS devices. They utilize frameworks like SpriteKit or game development engines like Unity to build games with rich graphics, sound effects, and gameplay mechanics.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon4.png" alt="#" /></span>
+                                <h6>IoT (INTERNET OF THINGS) APPS</h6>
+                                <p>With the rise of IoT devices, iOS app developers are building applications that interface with IoT devices, such as intelligent home systems, wearables, connected appliances, and other IoT-enabled devices. These apps facilitate control, monitoring, and interaction with the IoT ecosystem.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon5.png" alt="#" /></span>
+                                <h6>UTILITY APPS</h6>
+                                <p>iOS app developers are often hired to create utility applications that serve specific purposes, such as weather apps, calculator apps, note-taking apps, file managers, barcode scanners, language translators, and more. These apps provide convenient tools and services to iOS device users.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon6.png" alt="#" /></span>
+                                <h6>LOCATION-BASED APPS</h6>
+                                <p>iOS app developers leverage location-based services and APIs to develop applications that utilize GPS, Wi-Fi, or cellular network data to provide location-specific information and services. Examples include navigation, ride-sharing, travel, and tourism apps, and local business directories.</p>
+                            </div>
+                        </div> 
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon7.png" alt="#" /></span>
+                                <h6>SOCIAL NETWORKING APPS</h6>
+                                <p>iOS app developers contribute to creating social networking apps that facilitate communication, content sharing, and social interactions. These apps may include features like user profiles, messaging systems, news feeds, image and video sharing, and integration with social media platforms.</p>
+                            </div>
+                        </div>  
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/android_icon8.png" alt="#" /></span>
+                                <h6>EDUCATIONAL APPS</h6>
+                                <p>iOS app developers are involved in building educational apps targeting various audiences, such as children, students, or professionals. These apps offer learning content, quizzes, tutorials, language learning tools, and other educational resources.</p>
+                            </div>
+                        </div>
+                        
+
+
+
+                        
+
+
+
+                    <div class="row px-4">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <p class="text-justify text-white">These are just a few examples of the many use cases where iOS app developers are employed. Apple's iOS platform provides a vibrant ecosystem for app development, and the demand for skilled iOS app developers remains high in the mobile app development industry.</p>
+                            <p class="pb-0 text-justify text-white">Let Bizionic Technologies assist you in better understanding your project need and hire experienced, qualified, and prescreened iOS App Developers. <b>Kindly provide us with your brief requirement.</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="aboutProject_section hireFormSetting">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+
+                    <!-- form here  -->
+
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
---
+++ resources/views/templates/mobile-react-native-developer.blade.php
@@ -0,0 +1,204 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>REACT NATIVE DEVELOPERS</h1>
+                                    <h2 class="">
+                                        <strong>A</strong> LIGHTWEIGHT, INTERPRETED, <strong>AND</strong> JUST-IN-TIME <strong>COMPILED PROGRAMMING LANGUAGE WITH</strong> FIRST-CLASS FUNCTIONS.
+                                    </h2>
+
+                                        <p class="p_color text-justify">iOS app developers specialize in building applications specifically for Apple's iOS platform, which powers devices like iPhones, iPads, and iPods touch. They possess expertise in programming languages like Swift or Objective-C and deeply understand the iOS SDK (Software Development Kit) and related tools.</p>
+                                           <br>
+                                        <a href="/get-a-free-consultation" class="btn_default mt-2">Hire React Native Developer</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/reactNative_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init " data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3><strong>Here are some key areas where</strong> React Native    <strong>developers are commonly utilized:</strong></h2>
+
+                            
+                             
+                              
+                              
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon1.png" alt="#" /></span>
+                                <h6>CROSS-PLATFORM MOBILE APP DEVELOPMENT</h6>
+                                <p>React Native developers use the React Native framework to build native mobile applications that work seamlessly on iOS and Android platforms. They write code in JavaScript and utilize React Native's component-based architecture to create user interfaces that closely resemble native UI components.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon2.png" alt="#" /></span>
+                                <h6>SINGLE CODEBASE</h6>
+                                <p>React Native enables developers to use a single codebase to build mobile apps for multiple platforms. This significantly reduces development time and effort, as developers don't have to write separate codebases for iOS and Android. Instead, most of the code can be shared, and platform-specific code can be written when necessary.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon3.png" alt="#" /></span>
+                                <h6>NATIVE-LIKE PERFORMANCE</h6>
+                                <p>React Native apps leverage the native capabilities of the underlying platforms, allowing developers to build high-performance mobile applications. React Native bridges JavaScript code with native components, enabling seamless integration with device features such as camera, GPS, and push notifications.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon4.png" alt="#" /></span>
+                                <h6>UI/UX DESIGN</h6>
+                                <p>React Native developers create user interfaces using a combination of React Native components and native UI elements specific to each platform. They utilize React Native's declarative syntax and pre-built components to design intuitive and visually appealing mobile app interfaces.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon5.png" alt="#" /></span>
+                                <h6>THIRD-PARTY LIBRARIES AND APIS</h6>
+                                <p>React Native developers have access to a vast ecosystem of third-party libraries and APIs. They leverage these libraries to add functionality, integrate with services like social media platforms or payment gateways, and accelerate development by leveraging pre-built components and modules.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon6.png" alt="#" /></span>
+                                <h6>MINIMUM VIABLE PRODUCTS (MVPS)</h6>
+                                <p>React Native is often used for rapid prototyping and building Minimum Viable Products (MVPs). Its ability to share code across platforms allows for quick iterations and validation of app concepts, enabling faster time-to-market and cost-effectiveness.</p>
+                            </div>
+                        </div> 
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon7.png" alt="#" /></span>
+                                <h6>MAINTENANCE AND UPDATES</h6>
+                                <p>React Native developers are responsible for maintaining and updating mobile applications built with React Native. They ensure compatibility with new platform versions, apply security patches, fix bugs, and optimize performance to provide a smooth and up-to-date user experience.</p>
+                            </div>
+                        </div>  
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon8.png" alt="#" /></span>
+                                <h6>CODE REUSABILITY</h6>
+                                <p>React Native developers can reuse a significant portion of code between different platforms, saving time and effort in development. This code reusability allows for more efficient updates, bug fixes, and feature additions across iOS and Android versions of the app.</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon9.png" alt="#" /></span>
+                                <h6>HYBRID APP CONVERSION</h6>
+                                <p>React Native developers can convert existing web applications or hybrid apps to native-like mobile applications using React Native. By leveraging the existing web application codebase, they can quickly create mobile apps with improved performance and user experience.</p>
+                            </div>
+                        </div>  
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/reactNative_icon10.png" alt="#" /></span>
+                                <h6>APP STORE DEPLOYMENT</h6>
+                                <p>React Native developers are familiar with the app store deployment process for iOS and Android platforms. They ensure the app meets the requirements, follows submission guidelines, and handles platform-specific configurations and settings.</p>
+                            </div>
+                        </div>
+                        
+
+
+
+                        
+
+
+
+                    <div class="row px-4">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <p class="text-justify text-white">React Native developers are vital in building cross-platform mobile applications that offer a native-like experience. They utilize React Native's advantages of code reusability, performance, and access to native capabilities to develop mobile apps efficiently and effectively for both iOS and Android platforms.</p>
+                            <p class="pb-0 text-justify text-white">Let Bizionic Technologies assist you in better understanding your project need and hire experienced, qualified, and prescreened React Native Developers. <b>Kindly provide us with your brief requirement.</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="aboutProject_section hireFormSetting">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+
+                    <!-- form here  -->
+
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
