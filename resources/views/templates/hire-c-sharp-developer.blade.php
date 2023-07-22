eb6331e1d6d2777d701998b954df8fac5bdc9d27
Azeem <fzaidi@cloud-1.co>
2023-07-23 01:43:51 +0500

    inner hire pages

---
+++ resources/views/templates/hire-c-sharp-developer.blade.php
@@ -0,0 +1,177 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>C# DEVELOPERS</h1>
+                                    <h2 class="">
+                                        <strong>CREATE</strong> APPS AND PROGRAMS <strong>FOR</strong> WINDOWS <strong>OPERATING SYSTEMS BY UTILIZING</strong> .NET FRAMEWORK.
+                                    </h2>
+
+                                        <p class="p_color text-justify">Bizionic Technologies C# developers specialize in using the C# programming language to develop software applications and solutions. C# is a versatile and widely-used language developed by Microsoft, primarily used for building applications on the Microsoft .NET framework.</p>
+                                           <br>
+                                        <a href="/get-a-free-consultation" class="btn_default mt-2">Hire C# Developer</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/cSharp_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init " data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3><strong>Here are some key uses of</strong> C# <strong>development:</strong></h2>
+                            
+                              
+                              
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon1.png" alt="#" /></span>
+                                <h6>DESKTOP APPLICATION DEVELOPMENT</h6>
+                                <p>C# developers use frameworks like Windows Forms or Windows Presentation Foundation (WPF) to build desktop applications for Windows-based operating systems. These applications can range from simple utilities and tools to complex enterprise software solutions.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon2.png" alt="#" /></span>
+                                <h6>WEB APPLICATION DEVELOPMENT</h6>
+                                <p>C# developers leverage frameworks like ASP.NET or ASP.NET Core to create dynamic and interactive web applications. They utilize C# for server-side programming, handling request processing, database interactions, and business logic implementation. C# developers can also work with front-end technologies like HTML, CSS, and JavaScript to create full-stack web applications.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon3.png" alt="#" /></span>
+                                <h6>MOBILE APP DEVELOPMENT</h6>
+                                <p>With Xamarin, a cross-platform framework, C# developers can build mobile applications that run on iOS, Android, and Windows devices. Xamarin allows code sharing across different platforms, making developing and maintaining mobile apps easier using C#.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon4.png" alt="#" /></span>
+                                <h6>GAME DEVELOPMENT</h6>
+                                <p>C# is widely used in game development, particularly Unity. Unity supports C# scripting, enabling developers to create interactive and visually appealing games for various platforms, including consoles, PCs, mobile devices, and virtual reality (VR).</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon5.png" alt="#" /></span>
+                                <h6>BACKEND DEVELOPMENT</h6>
+                                <p>C# developers play a significant role in building the backend infrastructure and application logic. They utilize C# to develop APIs (Application Programming Interfaces), microservices, and server-side components that handle data processing, business rules, and integration with databases and external systems.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon6.png" alt="#" /></span>
+                                <h6>CLOUD DEVELOPMENT</h6>
+                                <p>C# developers leverage Microsoft Azure, a popular cloud computing platform, for building cloud-based applications. They use C# and Azure services to develop scalable and resilient cloud solutions, including serverless functions, web applications, and data processing pipelines.</p>
+                            </div>
+                        </div> 
+                        <div class="col-lg-3 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/cSharp_icon7.png" alt="#" /></span>
+                                <h6>SOFTWARE TOOLS AND LIBRARIES</h6>
+                                <p>C# developers contribute to developing software tools, libraries, and frameworks other developers use. They create reusable components, APIs, and frameworks that enhance productivity and simplify application development in various domains.</p>
+                            </div>
+                        </div> 
+
+
+                        
+
+
+
+                    <div class="row px-4">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <p class="text-justify text-white">Overall, C# developers utilize their C# programming language skills to build a wide range of applications, including desktop software, web applications, mobile apps, games, backend systems, and cloud-based solutions. In addition, c # offers a rich set of features, strong type safety, and integration with the extensive .NET ecosystem, making it a versatile choice for software development.</p>
+                            <p class="pb-0 text-justify text-white">Let Bizionic Technologies assist you in better understanding your project need and hire experienced, qualified, and prescreened C# Developers. <b>Kindly provide us with your brief requirement.</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="aboutProject_section hireFormSetting">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+
+                    <!-- form here  -->
+
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
--- resources/views/templates/hire-tableau-developer.blade.php
+++ resources/views/templates/hire-tableau-developer.blade.php
@@ -22,14 +22,14 @@
                         <div class="row align-items-center">
                             <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
                                 <div class=" custom_tittle text-left">
-                                    <h1>iOS APP DEVELOPERS</h1>
+                                    <h1>TABLEAU DEVELOPERS</h1>
                                     <h2 class="">
-                                        <strong>CREATE</strong> APPLICATIONS <strong>FOR MOBILE DEVICES POWERED BY</strong> APPLE'S iOS <strong>OPERATING SYSTEM</strong>.
+                                        <strong>CREATE A</strong> TABLEAU PLATFORM <strong>FOR</strong> DATA ANALYSIS, VISUALIZATION, <strong>AND</strong> BI PROCESS <strong>FOR YOUR</strong> BUSINESS NEEDS.
                                     </h2>
 
-                                        <p class="p_color text-justify">iOS app developers specialize in building applications specifically for Apple's iOS platform, which powers devices like iPhones, iPads, and iPods touch. They possess expertise in programming languages like Swift or Objective-C and deeply understand the iOS SDK (Software Development Kit) and related tools.</p>
+                                        <p class="p_color text-justify">Bizionic Technologies Tableau developers specialize in working with Tableau, a popular data visualization and business intelligence software. They use Tableau's features and capabilities to create interactive and visually appealing dashboards, reports, and data visualizations for businesses and organizations.</p>
                                            <br>
-                                        <a href="/get-a-free-consultation" class="btn_default mt-2">Hire iOS App Developer</a>
+                                        <a href="/get-a-free-consultation" class="btn_default mt-2">Hire Tableau Developer</a>
                                     
                                 </div>
                             </div>
@@ -37,7 +37,7 @@
 
                             <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
                                 <div class="industryNew_section1_avatar">
-                                    <span><img src="/bizionic/images/iosApp_avatar.png" alt="#" /></span>
+                                    <span><img src="/bizionic/images/tableau_avatar.png" alt="#" /></span>
                                 </div>
                             </div>
                         </div>
@@ -49,9 +49,9 @@
 
                     <div class="industryNew_section2_info aos-init " data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
                         <div class="custom_tittle">
-                            <h3>iOS app    <strong>developers are employed for various purposes and use cases, including:</strong></h2>
+                            <h3><strong>Here are the key areas where</strong> Tableau <strong>developers are involved:</strong></h2>
                             
-                             
+                            
                               
                               
                         </div>
@@ -73,30 +73,30 @@
                     <div class="row justify-content-center">
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon1.png" alt="#" /></span>
-                                <h6>CONSUMER MOBILE APPS</h6>
-                                <p>iOS app developers are hired to create mobile applications for end-users, catering to various industries such as social media, e-commerce, entertainment, travel, finance, health and fitness, and more. These apps are typically available on the Apple App Store for iOS device users.</p>
+                                <span><img src="/bizionic/images/tableau_icon1.png" alt="#" /></span>
+                                <h6>DATA ANALYSIS AND VISUALIZATION</h6>
+                                <p>Tableau developers leverage Tableau's data analysis and visualization capabilities to explore and analyze large datasets. They create interactive visualizations that help users understand complex data patterns, trends, and insights. Tableau developers work with various data sources, including databases, spreadsheets, and cloud-based platforms, to extract and transform data for visualization.</p>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon2.png" alt="#" /></span>
-                                <h6>ENTERPRISE MOBILE APPS</h6>
-                                <p>iOS app developers are crucial in developing custom applications for businesses and organizations. These apps serve internal purposes like employee management, sales tracking, inventory management, task management, and other business processes. Enterprise apps may also integrate with backend systems, databases, and other software solutions.</p>
+                                <span><img src="/bizionic/images/tableau_icon2.png" alt="#" /></span>
+                                <h6>DASHBOARD DEVELOPMENT</h6>
+                                <p>Tableau developers design and develop interactive dashboards that provide a consolidated view of key metrics and performance indicators. They create visually appealing layouts, charts, graphs, and other visual elements to present data meaningfully. In addition, tableau developers ensure that the dashboards are user-friendly, intuitive and enable users to interact with the data to gain deeper insights.</p>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon3.png" alt="#" /></span>
-                                <h6>GAMING APPS</h6>
-                                <p>iOS app developers with game development expertise are responsible for creating interactive and engaging games specifically for iOS devices. They utilize frameworks like SpriteKit or game development engines like Unity to build games with rich graphics, sound effects, and gameplay mechanics.</p>
+                                <span><img src="/bizionic/images/tableau_icon3.png" alt="#" /></span>
+                                <h6>DATA INTEGRATION</h6>
+                                <p>Tableau developers integrate data from multiple sources into Tableau for comprehensive analysis and visualization. They connect to various databases, cloud services, and other data repositories to extract relevant data and transform it into a format suitable for analysis. Tableau developers also perform data blending and merging to combine data from different sources into a unified view.</p>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon4.png" alt="#" /></span>
-                                <h6>IoT (INTERNET OF THINGS) APPS</h6>
-                                <p>With the rise of IoT devices, iOS app developers are building applications that interface with IoT devices, such as intelligent home systems, wearables, connected appliances, and other IoT-enabled devices. These apps facilitate control, monitoring, and interaction with the IoT ecosystem.</p>
+                                <span><img src="/bizionic/images/tableau_icon4.png" alt="#" /></span>
+                                <h6>REPORT GENERATION</h6>
+                                <p>Tableau developers generate customized reports that provide detailed analysis and insights to stakeholders. They create report templates, define report structures, and configure parameters for report generation. In addition, tableau developers ensure reports are visually appealing, well-structured, and contain relevant data visualizations and key performance indicators.</p>
                             </div>
                         </div>
 
@@ -105,32 +105,25 @@
 
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon5.png" alt="#" /></span>
-                                <h6>UTILITY APPS</h6>
-                                <p>iOS app developers are often hired to create utility applications that serve specific purposes, such as weather apps, calculator apps, note-taking apps, file managers, barcode scanners, language translators, and more. These apps provide convenient tools and services to iOS device users.</p>
+                                <span><img src="/bizionic/images/tableau_icon5.png" alt="#" /></span>
+                                <h6>DATA STORYTELLING</h6>
+                                <p>Tableau developers use Tableau's features to tell compelling stories with data. They combine data visualizations, text, and annotations to present data in a narrative format that effectively communicates insights and trends. In addition, tableau developers enhance the storytelling experience by incorporating interactive elements and guided analytics to engage users and facilitate data-driven decision-making.</p>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon6.png" alt="#" /></span>
-                                <h6>LOCATION-BASED APPS</h6>
-                                <p>iOS app developers leverage location-based services and APIs to develop applications that utilize GPS, Wi-Fi, or cellular network data to provide location-specific information and services. Examples include navigation, ride-sharing, travel, and tourism apps, and local business directories.</p>
+                                <span><img src="/bizionic/images/tableau_icon6.png" alt="#" /></span>
+                                <h6>PERFORMANCE OPTIMIZATION</h6>
+                                <p>Tableau developers optimize the performance of Tableau workbooks and dashboards to ensure fast and efficient data visualization and analysis. They implement data extraction techniques, data aggregation, and filtering strategies to enhance query performance. Tableau developers also fine-tune the dashboard design and layout to optimize loading times and responsiveness.</p>
                             </div>
                         </div> 
                         <div class="col-lg-3 col-md-6 provide_col">
                             <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon7.png" alt="#" /></span>
-                                <h6>SOCIAL NETWORKING APPS</h6>
-                                <p>iOS app developers contribute to creating social networking apps that facilitate communication, content sharing, and social interactions. These apps may include features like user profiles, messaging systems, news feeds, image and video sharing, and integration with social media platforms.</p>
+                                <span><img src="/bizionic/images/tableau_icon7.png" alt="#" /></span>
+                                <h6>COLLABORATION AND DEPLOYMENT</h6>
+                                <p>Tableau developers collaborate with other stakeholders, such as business analysts, data scientists, and project managers, to understand data requirements and align Tableau solutions with business needs. They assist in deploying Tableau dashboards and reports to production environments, ensuring seamless integration with existing systems and user access controls.</p>
                             </div>
-                        </div>  
-                        <div class="col-lg-3 col-md-6 provide_col">
-                            <div class="industryNew_section2_colsInfo">
-                                <span><img src="/bizionic/images/android_icon8.png" alt="#" /></span>
-                                <h6>EDUCATIONAL APPS</h6>
-                                <p>iOS app developers are involved in building educational apps targeting various audiences, such as children, students, or professionals. These apps offer learning content, quizzes, tutorials, language learning tools, and other educational resources.</p>
-                            </div>
-                        </div>
+                        </div>   
                         
 
 
@@ -141,8 +134,8 @@
 
                     <div class="row px-4">
                         <div class="custom_tittle pt-5 pb-0">
-                            <p class="text-justify text-white">These are just a few examples of the many use cases where iOS app developers are employed. Apple's iOS platform provides a vibrant ecosystem for app development, and the demand for skilled iOS app developers remains high in the mobile app development industry.</p>
-                            <p class="pb-0 text-justify text-white">Let Bizionic Technologies assist you in better understanding your project need and hire experienced, qualified, and prescreened iOS App Developers. <b>Kindly provide us with your brief requirement.</b></p>
+                            <p class="text-justify text-white">Tableau developers possess expertise in data visualization, data analysis, SQL, and Tableau's features and functionalities. They work closely with data teams, business users, and stakeholders to transform raw data into actionable insights and help organizations make data-driven decisions.</p>
+                            <p class="pb-0 text-justify text-white">Let Bizionic Technologies assist you in better understanding your project need and hire experienced, qualified, and prescreened Tableau developers. <b>Kindly provide us with your brief requirement.</b></p>
                         </div>
                     </div>
                 </div>
