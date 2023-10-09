62172dc3bdb8c965970abbf97d2f4ee4f9831345
Azeem <fzaidi@cloud-1.co>
2023-10-09 23:44:05 +0500

    15pages completed

---
+++ resources/views/templates/new-age-technologies-development.blade.php
@@ -0,0 +1,264 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>NEW AGE TECHNOLOGIES DEVELOPMENT</h1>
+                                    <h2 class="">
+                                        <strong>CREATE A NOTABLE</strong> ADVANCEMENT <strong>ON</strong> EXISTING 
+                                        SYSTEM <strong>AND</strong> IDENTIFY <strong>YOURSELF FROM</strong> RIVALS.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In an era dominated by technological innovation, staying updated with the latest advancements is no longer a choice but a necessity for businesses. Bizionic Technologies stands at the intersection of innovation and application, providing groundbreaking development services in new-age technologies.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_newAge_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>Here's an in-depth exploration of their diverse and impactful offerings:</h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon1.png" alt="#" /></span>
+                                <h6>BLOCKCHAIN DEVELOPMENT</h6>
+                                <p>Leveraging blockchain's decentralised nature, Bizionic facilitates transparent and secure transactions, revolutionising sectors from finance to healthcare.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon2.png" alt="#" /></span>
+                                <h6>AR/VR DEVELOPMENT</h6>
+                                <p>Whether it's enhancing the retail experience with AR or creating immersive VR gaming platforms, Bizionic crafts experiences that transcend traditional digital boundaries.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon3.png" alt="#" /></span>
+                                <h6>INTERNET OF THINGS (IoT) SOLUTIONS</h6>
+                                <p>Bizionic's expertise in IoT enables a more innovative, interconnected world, optimising industries from smart homes to intelligent urban planning.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon4.png" alt="#" /></span>
+                                <h6>QUANTUM COMPUTING DEVELOPMENT</h6>
+                                <p>Delving into the next frontier of computational prowess, Bizionic's quantum solutions promise unprecedented problem-solving capabilities.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon5.png" alt="#" /></span>
+                                <h6>5G NETWORK SOLUTIONS</h6>
+                                <p>Bizionic's 5G solutions facilitate seamless, lightning-fast communications, ensuring businesses are ready for future data demands.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon6.png" alt="#" /></span>
+                                <h6>ROBOTIC PROCESS AUTOMATION (RPA)</h6>
+                                <p>Through RPA, Bizionic helps businesses automate routine tasks, enhancing efficiency, reducing errors, and significant cost savings.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon7.png" alt="#" /></span>
+                                <h6>EDGE COMPUTING SERVICES</h6>
+                                <p>By prioritising data processing at the source, Bizionic's edge computing reduces latency, providing real-time insights and swift user experiences.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon8.png" alt="#" /></span>
+                                <h6>ADVANCED BIOMETRIC SYSTEMS</h6>
+                                <p>Whether facial, voice, or fingerprint recognition, Bizionic's biometric solutions ensure superior security, reducing fraud and enhancing user experience.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon9.png" alt="#" /></span>
+                                <h6>3D PRINTING INNOVATIONS</h6>
+                                <p>Bizionic Technologies accelerates product development, offering solutions from prototyping to production in the burgeoning realm of 3D printing.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_newAge_icon10.png" alt="#" /></span>
+                                <h6> NEUROMORPHIC COMPUTING DEVELOPMENT</h6>
+                                <p>Venturing into computing inspired by human neural structures, Bizionic is at the forefront of creating systems that mimic brain-like information processing.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">To sum it up, Bizionic Technologies is more than just a service provider; they're visionaries in the vast expanse of new-age tech. With a keen finger on the pulse of the latest trends and an unrelenting commitment to innovation, partnering with Bizionic ensures a future-forward trajectory, empowering businesses to navigate and lead in the digital age.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
