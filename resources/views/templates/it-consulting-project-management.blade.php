4a3b12987755adaa994d7a39d3f18c8e160e1e49
MHS <mumtaz@letwizard.com>
2023-10-10 04:28:32 +0000

    Squashed commit of the following
    commit 62172dc3 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 23:44:05 GMT+0500 (GMT+05:00) 
    
        15pages completed
    
    
    commit 06537064 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 22:51:47 GMT+0500 (GMT+05:00) 
    
        Merge branch 'develop' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/bizionic-nova into fe44
    
    
    commit 7a381e81 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 22:50:57 GMT+0500 (GMT+05:00) 
    
        update service page
    
    
    commit f1941e5e 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 09:50:15 GMT+0500 (GMT+05:00) 
    
        10pages complete of services dropdown
    
    
    commit bad9fcbe 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Wed Oct 04 2023 22:46:48 GMT+0500 (GMT+05:00) 
    
        technology pages complete
    
    
    commit c5f302f8 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Wed Oct 04 2023 00:01:25 GMT+0500 (GMT+05:00) 
    
        update 9 more pages

---
+++ resources/views/templates/it-consulting-project-management.blade.php
@@ -0,0 +1,264 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>IT CONSULTING AND PROJECT MANAGEMENT</h1>
+                                    <h2 class="">
+                                        ADVISE <strong>AND</strong> MANAGE <strong>CLIENTS</strong> TECHNOLOGY STRATEGIES 
+                                        <strong>AND</strong> ALIGN <strong>THEM WITH THEIR</strong> BUSINESSES NEEDS.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In the rapidly evolving world of information technology, keeping pace with the latest trends, technologies, and best practices can be arduous for businesses. Bizionic Technologies emerges as a guiding light in this realm, offering a suite of IT consulting and project management services tailored to drive success.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_itConsulting_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>Here's a comprehensive look into their expertly crafted offerings:</h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon1.png" alt="#" /></span>
+                                <h6>IT STRATEGY AND ROADMAP DEVELOPMENT</h6>
+                                <p>With a keen understanding of present scenarios and future aspirations, Bizionic assists businesses in crafting robust IT strategies and roadmaps, ensuring technological endeavours align with overarching business goals.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon2.png" alt="#" /></span>
+                                <h6>TECHNOLOGY AUDITS AND ASSESSMENTS</h6>
+                                <p>A well-crafted cloud infrastructure is pivotal for performance. Bizionic designs robust, scalable, and resilieaBizionic conducts thorough technology audits, assessing the current IT landscape of an organisation, identifying bottlenecksnt cloud architectures, ensuring optimal performance and minimal downtime.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon3.png" alt="#" /></span>
+                                <h6>PROJECT MANAGEMENT AND DELIVERY</h6>
+                                <p>With a proven track record, Bizionic’s seasoned project managers oversee projects from inception to completion, ensuring timely delivery, within budget, and in line with quality standards.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon4.png" alt="#" /></span>
+                                <h6>DIGITAL TRANSFORMATION CONSULTATION</h6>
+                                <p>In today’s digitised era, staying relevant requires transformation. Bizionic provides expert guidance on digital transformation initiatives, ensuring businesses remain competitive and agile.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon5.png" alt="#" /></span>
+                                <h6>IT GOVERNANCE AND COMPLIANCE</h6>
+                                <p>Bizionic emphasises robust IT governance, ensuring that technology decisions and practices align with business needs while meeting regulatory and compliance mandates.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon6.png" alt="#" /></span>
+                                <h6>RISK MANAGEMENT AND MITIGATION</h6>
+                                <p>Recognizing the inherent risks in IT endeavours, Bizionic offers strategic risk assessment and crafts bespoke mitigation plans, safeguarding business interests.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon7.png" alt="#" /></span>
+                                <h6>VENDOR AND TECHNOLOGY SELECTION</h6>
+                                <p>With many technology vendors in the market, making the right choice can be overwhelming. Bizionic guides businesses in vendor and technology selection, ensuring compatibility with business needs.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon8.png" alt="#" /></span>
+                                <h6>BUSINESS PROCESS REENGINEERING</h6>
+                                <p>Bizionic examines existing business processes, suggesting and implementing refinements and innovations that leverage technology for efficiency and growth.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon9.png" alt="#" /></span>
+                                <h6>IT BUDGETING AND FINANCIAL PLANNING</h6>
+                                <p>Bizionic assists businesses in IT budgeting, ensuring that technology investments are prudent and yield the maximum return on investment.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_itConsulting_icon10.png" alt="#" /></span>
+                                <h6> CONTINUOUS SUPPORT AND ADVISORY</h6>
+                                <p>Beyond initial consultations and project management, Bizionic offers continuous support and advisory services, ensuring businesses stay updated, optimised, and ahead of the curve.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">In conclusion, Bizionic Technologies epitomises excellence in IT consulting and project management. Their holistic, tailored approach, backed by years of expertise, ensures that businesses, whether startups or established enterprises, navigate IT with confidence and success. By partnering with Bizionic, companies not only secure technological prowess but also a strategic advantage in their respective industries.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
---
+++ resources/views/templates/quality-assurance-qa.blade.php
@@ -0,0 +1,265 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>QUALITY ASSURANCE</h1>
+                                    <h2 class="">
+                                        <strong>MONITOR ALL ENGINEERING</strong> PROCEDURES, 
+                                        TECHNIQUES, <strong>AND</strong> WORKAROUNDS <strong>TO MAKE</strong> SURE <strong>THAT 
+                                        THE</strong> PRODUCT COMPLIES <strong>WITH THE</strong> ESTABLISHED STANDARDS.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In software and technology, ensuring quality is more than just a checkbox; it's a brand's promise to its users. Quality assurance (QA) is pivotal in upholding this promise as the vanguard against defects, glitches, and sub-par user experiences. Bizionic Technologies, with its unwavering commitment to excellence, stands tall as a leader in quality assurance services.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_qAssurance_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3><strong>Let's delve into the myriad of</strong> QA offerings <strong>that Bizionic brings to the table:</strong></h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon1.png" alt="#" /></span>
+                                <h6>COMPREHENSIVE QA STRATEGY DEVELOPMENT</h6>
+                                <p>Bizionic's team of experts collaborates closely with businesses to craft bespoke QA strategies, ensuring each testing phase aligns with overarching business objectives and software requirements.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon2.png" alt="#" /></span>
+                                <h6>FUNCTIONAL TESTING</h6>
+                                <p>This entails validating every software function ensuring all features operate as per the defined specifications and requirements.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon3.png" alt="#" /></span>
+                                <h6>PERFORMANCE AND LOAD TESTING</h6>
+                                <p>Bizionic assesses how the software performs under stress, simulating various user loads to ensure smooth operation during peak demand.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon4.png" alt="#" /></span>
+                                <h6>USABILITY AND USER EXPERIENCE TESTING</h6>
+                                <p>Prioritizing the end-user experience, Bizionic conducts tests to ensure intuitive navigation, coherent layouts, and a user-friendly interface.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon5.png" alt="#" /></span>
+                                <h6>AUTOMATION TESTING</h6>
+                                <p>Using state-of-the-art tools, Bizionic ensures repetitive and complex tasks are automated, increasing testing accuracy and expediting the QA process.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon6.png" alt="#" /></span>
+                                <h6>SECURITY AND VULNERABILITY TESTING</h6>
+                                <p>Recognizing the increasing cybersecurity threats, Bizionic identifies and remedies potential software vulnerabilities, ensuring robust protection against malicious attacks.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon7.png" alt="#" /></span>
+                                <h6>CROSS-PLATFORM AND CROSS-BROWSER TESTING</h6>
+                                <p>Bizionic ensures that software applications deliver a consistent user experience, irrespective of the platform, operating system, or browser.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon8.png" alt="#" /></span>
+                                <h6>LOCALIZATION AND GLOBALIZATION TESTING</h6>
+                                <p>As businesses target global audiences, ensuring software is culturally and linguistically apt becomes paramount. Bizionic’s testing ensures software is globally capable and locally relevant.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon9.png" alt="#" /></span>
+                                <h6>QA IN DEVOPS AND CONTINUOUS INTEGRATION</h6>
+                                <p>Embracing modern software development approaches, Bizionic integrates QA seamlessly into the DevOps cycle, ensuring rapid yet quality releases.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_qAssurance_icon10.png" alt="#" /></span>
+                                <h6> 24/7 QA SUPPORT AND MAINTENANCE</h6>
+                                <p>Beyond the initial testing phases, Bizionic offers round-the-clock QA support, ensuring software remains glitch-free, updated, and optimised as it evolves.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">In essence, Bizionic Technologies is not just a QA service provider but a partner in a brand’s journey to excellence. With a keen eye for detail, unparalleled expertise, and a relentless pursuit of perfection, they ensure that every software product meets the highest quality standards. Partnering with Bizionic guarantees impeccable software and the peace of mind that comes with knowing you're in the best hands in the industry.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
