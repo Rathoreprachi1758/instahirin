5dcb29551f83ee6d42f1540ac6569e2058d194a3
MHS <mumtaz@letwizard.com>
2023-10-11 05:32:38 +0000

    Squashed commit of the following
    commit 104a4b4b 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Wed Oct 11 2023 09:33:14 GMT+0500 (GMT+05:00) 
    
        4 more pages
    
    
    commit 62172dc3 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 23:44:05 GMT+0500 (GMT+05:00) 
    
        15pages completed
    
    
    commit 06537064 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 22:51:47 GMT+0500 (GMT+05:00) 
    
        Merge branch 'develop' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/bizionic-nova into fe44
    
    
    commit 7a381e81 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 22:50:57 GMT+0500 (GMT+05:00) 
    
        update service page
    
    
    commit f1941e5e 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Mon Oct 09 2023 09:50:15 GMT+0500 (GMT+05:00) 
    
        10pages complete of services dropdown
    
    
    commit bad9fcbe 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Wed Oct 04 2023 22:46:48 GMT+0500 (GMT+05:00) 
    
        technology pages complete
    
    
    commit c5f302f8 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Wed Oct 04 2023 00:01:25 GMT+0500 (GMT+05:00) 
    
        update 9 more pages

---
+++ resources/views/templates/technology-training-workshops.blade.php
@@ -0,0 +1,266 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>TECHNOLOGY TRAINING AND WORKSHOPS</h1>
+                                    <h2 class="">
+                                        <strong>USERS CAN BE</strong> TRAINED <strong>BY</strong> EDUCATIING, 
+                                        INSTRUCTING <strong>OR HELD A</strong> WORKSHOP <strong>ABOUT A 
+                                        PARTICULAR</strong> SOFTWARE, PRODUCT, <strong>OR</strong> SYSTEM, 
+                                        <strong>FOR</strong> BUSINESS <strong>DEVELOPMENT</strong> ENDEAVOUR.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In the swiftly evolving world of technology, staying updated and acquiring new skills is paramount. Recognising this crucial need, Bizionic Technologies offers a range of Technology Training and Workshop Services to bolster technical proficiency for individuals and organisations alike.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_techWorkshop_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>Here's a closer look at their comprehensive offerings:</h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon1.png" alt="#" /></span>
+                                <h6>CUSTOMIZED TRAINING PROGRAMS</h6>
+                                <p>Bizionic designs tailored training modules to meet specific organisational needs, ensuring maximum knowledge retention and applicability.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon2.png" alt="#" /></span>
+                                <h6>HANDS-ON WORKSHOPS</h6>
+                                <p>Focusing on experiential learning, Bizionic conducts interactive workshops providing participants with hands-on experience on various technological tools and platforms.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon3.png" alt="#" /></span>
+                                <h6>TECH BOOTCAMPS</h6>
+                                <p>For those looking to dive deep and fast, Bizionic offers tech boot camps—short, intensive training sessions that comprehensively cover various tech topics.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon4.png" alt="#" /></span>
+                                <h6>CERTIFICATION PROGRAMS</h6>
+                                <p>Bizionic collaborates with industry leaders to offer certification programs, validating participants' proficiency in specific technologies.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon5.png" alt="#" /></span>
+                                <h6>CORPORATE TRAINING</h6>
+                                <p>Recognizing the dynamic needs of businesses, Bizionic offers corporate training sessions, ensuring teams stay updated with the latest technological advancements.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon6.png" alt="#" /></span>
+                                <h6>WEBINARS & ONLINE CLASSES</h6>
+                                <p>Leveraging the power of digital platforms, Bizionic provides webinars and online classes, ensuring continuous learning irrespective of geographical boundaries.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon7.png" alt="#" /></span>
+                                <h6>MENTORSHIP PROGRAMS</h6>
+                                <p>Experienced tech professionals at Bizionic offer mentorship programs, providing guidance, insights, and direction to budding tech enthusiasts.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon8.png" alt="#" /></span>
+                                <h6>SPECIALIZED TECHNOLOGY TRACKS</h6>
+                                <p>From AI to Blockchain, Bizionic offers specialised training tracks catering to specific technological domains, ensuring in-depth knowledge acquisition.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon9.png" alt="#" /></span>
+                                <h6>POST-TRAINING SUPPORT</h6>
+                                <p>Understanding that learning doesn't end at the workshop, Bizionic offers post-training support, answering queries and providing additional resources.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_techWorkshop_icon10.png" alt="#" /></span>
+                                <h6> TRAINING MATERIALS & RESOURCES</h6>
+                                <p>Participants are equipped with a rich repository of training materials, e-books, and tutorials, ensuring they can revisit and reinforce their learning at their own pace.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">In today's tech-driven landscape, knowledge and skill acquisition can separate individuals and organisations. With Bizionic Technologies at the helm of your learning journey, you're equipped with the latest knowledge and empowered with the confidence to apply it effectively. Whether you're a novice looking to get started or a seasoned professional aiming to enhance your skills, Bizionic's Technology Training and Workshop Services have got you covered.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
