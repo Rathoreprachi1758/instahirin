a18162a7ce73cdf53b412b78338b0f0384adee91
Azeem <fzaidi@cloud-1.co>
2023-10-12 23:22:23 +0500

    total 15 pages more

---
+++ resources/views/templates/content-strategy-planning.blade.php
@@ -0,0 +1,265 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>CONTENT STRATEGY AND PLANNING</h1>
+                                    <h2 class="">
+                                        <strong>TO</strong> ENGAGE MORE <strong>AUDIENCES,</strong> 
+                                        CREATE, BUILD, DELIVER, <strong>AND</strong> CONTROL 
+                                        <strong>ON</strong> USEFUL, USEABLE, <strong>AND</strong> EFFECTIVE CONTENT.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In the dynamic realm of digital marketing, content stands as a king, dictating brand narratives, user engagements, and conversion paths. Bizionic Technologies, with its deep-rooted expertise in digital landscapes, has emerged as a pioneer in content strategy and planning. Their comprehensive suite in this domain ensures that brands narrate stories and craft legacies.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_contentPlaning_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>Let’s delve into the detailed services they offer:</h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon1.png" alt="#" /></span>
+                                <h6>CONTENT AUDIT & ANALYSIS</h6>
+                                <p>Through a meticulous review of your current content, Bizionic pinpoints gaps, redundancies, and opportunities, laying the foundation for a streamlined strategy.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon2.png" alt="#" /></span>
+                                <h6>AUDIENCE PERSONA DEVELOPMENT</h6>
+                                <p>By understanding and defining audience personas, they ensure the content resonates deeply, driving both engagement and loyalty.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon3.png" alt="#" /></span>
+                                <h6>CONTENT CALENDAR CREATION</h6>
+                                <p>A detailed content calendar ensures a consistent flow of content, keeping audiences engaged and brands at the top of their mind.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon4.png" alt="#" /></span>
+                                <h6>PLATFORM-SPECIFIC STRATEGY</h6>
+                                <p>Recognizing the unique nuances of different platforms, from blogs to social media, they craft strategies tailored for optimal performance.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon5.png" alt="#" /></span>
+                                <h6>SEO-ALIGNED CONTENT PLANNING</h6>
+                                <p>By integrating SEO insights into content planning, Bizionic ensures that your content engages and ranks, driving organic traffic.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon6.png" alt="#" /></span>
+                                <h6>MULTIMEDIA CONTENT STRATEGY</h6>
+                                <p>They understand the power of diverse content types, from articles to videos, and integrate them for a rich, multi-layered user experience.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon7.png" alt="#" /></span>
+                                <h6>COMPETITIVE ANALYSIS</h6>
+                                <p>By studying competitors, Bizionic identifies industry content trends and potential gaps, positioning your brand as an industry thought leader.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon8.png" alt="#" /></span>
+                                <h6>GOAL SETTING & KPI DEVELOPMENT</h6>
+                                <p>They set clear content goals aligned with your business objectives and establish KPIs, ensuring trackable and tangible results.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon9.png" alt="#" /></span>
+                                <h6>CONTENT REPURPOSING STRATEGY</h6>
+                                <p>Recognizing the value of evergreen content, they devise strategies to repurpose and reintroduce content, maximising ROI.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_contentPlaning_icon10.png" alt="#" /></span>
+                                <h6> CRISIS MANAGEMENT PLANNING</h6>
+                                <p>Content crises can flare up quickly in the digital age. Bizionic ensures you're prepared with strategies to address and defuse potential content-related issues.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">By entrusting your content strategy and planning to Bizionic Technologies, you're equipping your brand with a blueprint for digital dominance. They blend art with analytics, ensuring your content is captivating and conversion-centric in the maze of digital content; having Bizionic as your guide provides your brand's voice that doesn't just echo but leaves a lasting impact.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
