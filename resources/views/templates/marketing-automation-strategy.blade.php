6f7ce83fec400a9d9f066828efbe722dd795c40a
Azeem <fzaidi@cloud-1.co>
2023-10-15 18:13:47 +0500

    services pages completed

---
+++ resources/views/templates/marketing-automation-strategy.blade.php
@@ -0,0 +1,265 @@
+   
+   <div class="bg_cfd1d2">
+
+ 
+   
+   <!-- main banner img -->
+   
+   <div class="mainBanner_layout">
+    <span><img src="/bizionic/images/hire_main_banner.png" alt="#" /></span>
+   </div>
+
+
+
+
+    <div class="onHire_immerPage">
+        <div class="industryNew_section  bg_f2f2f2">
+            <div class="auto_container">
+                <div class="industryNew_section_detail">
+
+
+                    <div class="industryNew_section1">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class=" custom_tittle text-left">
+                                    <h1>MARKETING AUTOMATION STRATEGY</h1>
+                                    <h2 class="">
+                                        <strong>USE MARKETING</strong> AUTOMATION STRATEGY <strong>TO AUTOMATE</strong> 
+                                        REPETITIVE MARKETING TASKS <strong>SUCH AS</strong> EMAIL MARKETING, 
+                                        SOCIAL MEDIA POSTING, AD CAMPAIGNS, <strong>AND</strong> MUCH MORE.
+
+                                    </h2>
+
+                                        <p class="p_color text-justify">In today's hyper-digital era, businesses are constantly looking for ways to streamline processes, enhance efficiency, and achieve more with less. One such boon for modern marketers is marketing automation. However, to truly harness its potential, you need a robust strategy. This is where Bizionic Technologies comes into play with its premier Marketing Automation Strategy Services.</p>
+                                        <br>
+                                        <a href="#gotoContact" class="btn_default mt-4">Contact Us</a>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6 col-md-6 aos-init " data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease">
+                                <div class="industryNew_section1_avatar">
+                                    <span><img src="/bizionic/images/s_marketingStrategy_avatar.png" alt="#" /></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div class="industryNew_section2_info aos-init" data-aos="fade-up" data-aos-duration="1500" data-aos-easing="ease">
+                        <div class="custom_tittle">
+                            <h3>Let's dive into what they offer:</h3>
+  
+
+
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        
+
+        <div class="provider_section">
+            <div class="auto_container">
+                <div class="industryNew_section2_cols">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon1.png" alt="#" /></span>
+                                <h6>AUTOMATION NEEDS ANALYSIS</h6>
+                                <p>Before diving into tools and tactics, Bizionic starts by understanding your unique business needs and objectives, laying a solid foundation for the automation journey ahead.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon2.png" alt="#" /></span>
+                                <h6>TOOL SELECTION AND INTEGRATION</h6>
+                                <p>The market is flooded with marketing automation tools. Bizionic's expertise helps you navigate this ocean, selecting and integrating the tool that aligns best with your business model.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon3.png" alt="#" /></span>
+                                <h6>CUSTOMER JOURNEY MAPPING</h6>
+                                <p>Bizionic crafts detailed customer journey maps, ensuring each touchpoint is optimised and automated to gently guide prospects down the conversion funnel.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon4.png" alt="#" /></span>
+                                <h6>LEAD SCORING AND SEGMENTATION</h6>
+                                <p>By segmenting your audience and scoring leads based on interactions, Bizionic ensures your messaging remains relevant, increasing engagement and conversion rates.</p>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon5.png" alt="#" /></span>
+                                <h6>EMAIL MARKETING AUTOMATION</h6>
+                                <p>From welcome sequences to cart abandonment reminders, Bizionic sets up automated email campaigns that resonate and drive action.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon6.png" alt="#" /></span>
+                                <h6>MULTI-CHANNEL CAMPAIGN COORDINATION</h6>
+                                <p>Whether social media, email, or your website, Bizionic ensures consistent channel messaging, enhancing brand recall and engagement.</p>
+                            </div>
+                        </div>
+                
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon7.png" alt="#" /></span>
+                                <h6>PERFORMANCE ANALYTICS AND OPTIMIZATION</h6>
+                                <p>Automation is not a set-and-forget affair. Bizionic provides regular insights on campaign performance, fine-tuning strategies for constant improvement.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon8.png" alt="#" /></span>
+                                <h6>TRAINING AND WORKSHOPS</h6>
+                                <p>Beyond just setting up, Bizionic believes in empowering your team. They offer training sessions and workshops to ensure you're equipped to make the most of your automation tools.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon9.png" alt="#" /></span>
+                                <h6>A/B TESTING AND ITERATION</h6>
+                                <p>Through rigorous A/B testing, Bizionic identifies what truly works for your audience, constantly refining campaigns for optimal results.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 provide_col">
+                            <div class="industryNew_section2_colsInfo">
+                                <span><img src="/bizionic/images/s_marketingStrategy_icon10.png" alt="#" /></span>
+                                <h6> POST-IMPLEMENTATION SUPPORT</h6>
+                                <p>Automation might seem daunting, but with Bizionic's post-implementation support, you're always there. They offer continuing guidance, ensuring your marketing automation journey is smooth and successful.</p>
+                            </div>
+                        </div>
+                        
+  
+  
+                        </div> 
+                        
+
+
+
+                    <div class="row px-3">
+                        <div class="custom_tittle pt-5 pb-0">
+                            <h4 class="text-white text-left d-none">Host Memorable Events with Bizionic!</h4>
+                            <p class=" text-justify text-white d-none">Partner with Bizionic Marketing Solutions to elevate your events and create unforgettable experiences for your attendees. Our team of event management experts is dedicated to delivering flawless execution and ensuring that your brand's vision comes to life.</p>
+                            <p class="pb-0 text-justify text-white">In an age where personalisation is critical and efficiency is king, marketing automation is a pivotal element for businesses aiming for growth. With Bizionic Technologies' expert strategy services, you've ensured a partner that understands the nuances of automation, guiding your business towards a brighter, more efficient future. It's about leveraging technology smartly; with Bizionic, you're in capable hands.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ 
+
+
+
+        <div class="aboutProject_section marketingFormSetting" id="gotoContact">
+            <div class="auto_container">
+
+                <div class="contactUs_detail">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4 col-md-5  aos-init " data-aos="fade-right" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+                            <div class="biz_brief">
+                                <div class="biz_brief_info">
+                                    <h3>BIZIONIC in Brief:</h3>
+                                    <ul>
+                                        <li>
+                                            <strong>10+ Years of Expertise</strong>
+                                            <p>IT powerhouse of Hyderabad India</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>150+ Developers and Marketers</strong>
+                                            <p>In-house, Min 5+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>25+ Project Manager's Team</strong>
+                                            <p>10+ Years of Experience</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>50M+ Users of Solutions</strong>
+                                            <p>Our Team Developed</p>
+                                        </li>
+
+                                        <li>
+                                            <strong>250+ Projects Accomplished</strong>
+                                        </li>
+
+                                        <li>
+                                            <strong>1000K+ Development Hours</strong>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-8 col-md-7   aos-init " data-aos="fade-left" data-aos-duration="800"
+                            data-aos-easing="ease-out-cubic">
+
+                            <!-- form here  -->
+                            <div class="contactForm_page getFree">
+                                <div class="custom_tittle text-left">
+                                    <h2 class="p_color">Contact Us:</h2>
+                                    <p>Please fill the form and our representative will get back to you.</p>
+                                </div>
+                                <contact-us-component></contact-us-component>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+ 
+
+
+
+ <!-- our client Bizionic section -->
+
+
+ <client-component></client-component>
+
+ 
+
+
+     <!-- Services provided section -->
+
+
+     @include('partials.services') 
+
+
+
+
+
+
+</div>
