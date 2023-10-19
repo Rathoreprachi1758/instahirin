e5e8f1e7c20961035fb8ba4a70fb5a6406dfac6c
Azeem <fzaidi@cloud-1.co>
2023-10-19 23:09:24 +0500

    complete statup page and apply form

---
+++ resources/views/templates/funding-apply-now-form.blade.php
@@ -0,0 +1,304 @@
+   
+   <div class="fundingApplyForm_page">
+        <div class="auto_container">
+            <div class="fundingApplyForm_pageDetail">
+                <div class="custom_tittle">
+                    <h3 class="text-left"><strong>Please fill the initial information for </strong>Start-up Funding Initiative.</h3>
+                </div>
+
+
+                <div class="fundingApplyForm_info">
+
+
+                <!-- section 1  -->
+
+                    <div class="fundingApplyForm_info_section">
+                        <div class="custom_tittle">
+                            <h5 class="text-left pb-0">Name <strong>(required)</strong></h5>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>First Name</strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Last Name</strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Title <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Email <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Company <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>HQ <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="row">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="fundingForm_col">
+                                    <strong>Website <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <br>
+                        <hr>
+                        <br>
+                    </div>
+
+
+
+
+                    <!-- section 2  -->
+
+                    <div class="fundingApplyForm_info_section">
+                        <div class="custom_tittle">
+                            <h5 class="text-left pb-0">Social Media </h5>
+                        </div> 
+
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>LinkedIn <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Instagram </strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+
+                        <div class="row">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="fundingForm_col">
+                                    <strong>Twitter</strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <br>
+                        <hr>
+                        <br>
+                    </div>
+
+
+
+
+
+                    <!-- section 3  -->
+
+                    <div class="fundingApplyForm_info_section">
+                        <div class="custom_tittle">
+                            <h5 class="text-left pb-0">Financials </h5>
+                        </div> 
+
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Are you actively raising capital? <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_select">
+                                        <select>
+                                            <option value="Yes">Yes</option>
+                                            <option value="No">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Have you ever raised venture capital? <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_select">
+                                        <select>
+                                            <option value="Yes">Yes</option>
+                                            <option value="No">No</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>How much have you raised? <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_select">
+                                        <select>
+                                            <option value="$100,000 - $250,000">$100,000 - $250,000</option>
+                                            <option value="$200,000 - $250,000">$200,000 - $250,000</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Valuation <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_select">
+                                        <select>
+                                            <option value="$10">$10</option>
+                                            <option value="$20">$20</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+                        <br>
+                        <hr>
+                        <br>
+                    </div>
+
+
+
+
+                     <!-- section 4  -->
+
+                     <div class="fundingApplyForm_info_section">
+                        <div class="custom_tittle">
+                            <h5 class="text-left pb-0">Company </h5>
+                        </div> 
+
+
+
+                        <div class="row">
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>What is your enabling technology? <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-6  col-md-6">
+                                <div class="fundingForm_col">
+                                    <strong>Industry <sup class="text-danger">*</sup></strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+
+                        <div class="row">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="fundingForm_col">
+                                    <strong>Challanges your company is solving? <sup class="text-danger">*</sup></strong>
+                                    <p class="pb-2">Please describe the challanges you’re solving in only 2 to 3 sentences.</p>
+                                    <div class="project_form_textarea">
+                                        <textarea placeholder="" name="message" id=""></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+
+                        <div class="row">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="fundingForm_col">
+                                    <strong>Who is the core buyer/user of your solution?</strong>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+                        <div class="row">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="fundingForm_col">
+                                    <strong>Who are your investors? <sup class="text-danger">*</sup></strong>
+                                    <p class="pb-2">If you’re bootstrapped, please place N/A in field below.</p>
+                                    <div class="project_form_field">
+                                        <input type="" placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+
+
+                        <div class="row pt-3">
+                            <div class="col-lg-12  col-md-12">
+                                <div class="applyBtn">
+                                    <input type="submit" class="btn_default" value="Submit" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+   </div>
