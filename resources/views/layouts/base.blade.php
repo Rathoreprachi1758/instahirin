3f41ef95f1a85f1f194ff4cd919dc9182f4ed148
MHS <mumtaz@letwizard.com>
2023-11-09 11:19:04 +0000

    Squashed commit of the following
    commit 903a7728 
    Author: Azeem <fzaidi@cloud-1.co> 
    Date: Thu Nov 09 2023 09:36:11 GMT+0500 (Pakistan Standard Time) 
    
        portfolio page

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -166,7 +166,7 @@
                                 <div class="trendingNav_links">
                                     <ul>
                                         <li><a href="/industries/industries-we-serve/industries/startups">Startup Funding</a></li>
-                                        <li><a href="#">Trending News</a></li>
+                                        <li><a href="/hire/developer/front-end-developement/portfolio">Portfolio</a></li>
                                         <li><a href="/hire/developer/front-end-developement/blogs">Blogs</a></li>
                                     </ul>
                                 </div>
