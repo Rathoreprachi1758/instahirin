9c875400b6868e4f9bd7e39556185ac023f6ffe7
MHS <mumtaz@letwizard.com>
2024-05-03 11:30:41 +0000

    Squashed commit of the following
    commit a43a9f7f 
    Author: khanallama <allama@bizionictech.com> 
    Date: Fri May 03 2024 16:20:58 GMT+0500 (Pakistan Standard Time) 
    
        [TASK]created login with google,github,apple,linkdin

---
+++ app/Http/Controllers/SocialLoginController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialLoginController extends Controller
+{
+    public function googleLogin()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callBack(Request $request): \Illuminate\Foundation\Application|\Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse|\Illuminate\Contracts\Foundation\Application
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+        } catch (\Exception $e) {
+            return redirect('/loginpage')->with('error', 'Google authentication failed.');
+        }
+        $existingUser = User::where('email', $googleUser->email)->first();
+        if ($existingUser) {
+            auth()->login($existingUser);
+//            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        } else {
+            $user = new User();
+            $user->name = $googleUser->name;
+            $user->email = $googleUser->email;
+            $user->save();
+            auth()->login($user);
+
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+    }
+
+    public function linkedinLogin()
+    {
+//        $a= Socialite::driver('linkedin')->redirect();
+//        dd ($a);
+        return Socialite::driver('linkedin')->redirect();
+    }
+
+    public function linkedinCallBack()
+    {
+        try {
+
+            $user = Socialite::driver('linkedin')->user();
+
+            dd($user);
+//            $linkedinUser = User::where('oauth_id', $user->id)->first();
+//            dd($linkedinUser);
+//            $user = Socialite::driver('linkedin')->user();
+//
+//            $linkedinUser = User::where('oauth_id', $user->id)->first();
+//
+//            if($linkedinUser){
+//
+//                Auth::login($linkedinUser);
+//
+//                return redirect('/dashboard');
+//
+//            }else{
+//                $user = User::create([
+//                    'name' => $user->name,
+//                    'email' => $user->email,
+//                    'oauth_id' => $user->id,
+//                    'oauth_type' => 'linkedin',
+//                    'password' => encrypt('admin12345')
+//                ]);
+//
+//                Auth::login($user);
+//
+//                return redirect('/dashboard');
+//            }
+
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+
+
+    }
+
+    public function githubLogin()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function githubCallBack(Request $request)
+    {
+        try {
+            $githubUser = Socialite::driver('github')->user();
+//            dd($githubUser);
+        } catch (\Exception $e) {
+            return redirect('/loginpage')->with('error', 'Google authentication failed.');
+        }
+        $existingUser = User::where('email', $githubUser->email)->first();
+        if ($existingUser) {
+            auth()->login($existingUser);
+            return redirect()->intended('dashboard');
+        } else {
+            $user = new User();
+            $user->name = $githubUser->name;
+            $user->email = $githubUser->email;
+            $user->save();
+            auth()->login($user);
+
+            return redirect()->intended('dashboard');
+        }
+    }
+
+    public function appleCallBack(Request $request)
+    {
+
+        $idToken = json_decode($request->detailData, true)['authorization']['id_token'];
+        $userData = json_decode($request->detailData, true)['user'] ?? null;
+        $tokenParts = explode('.', $idToken);
+        $decodedPayload = base64_decode($tokenParts[1]);
+        $payloadData = json_decode($decodedPayload, true);
+        $payloadDataSub = $payloadData['sub'];
+
+        $existingUser = User::where('apple_sub', $payloadDataSub)->first();
+
+        if ($userData != null) {
+            $user = new User();
+            $user->name = $userData['name']['firstName'] . '' . $userData['name']['lastName'];
+            $user->email = $userData['email'];
+            $user->apple_sub = $payloadDataSub;
+            $user->save();
+            auth()->login($user);
+            return redirect()->intended('dashboard');
+        } elseif ($existingUser) {
+            auth()->login($existingUser);
+            return redirect()->intended('dashboard');
+        } elseif (!$existingUser) {
+            $user = new User();
+            $user->apple_sub = $payloadDataSub;
+            $user->save();
+            auth()->login($user);
+            return redirect()->intended('dashboard');
+        }
+        dd($request);
+
+        dd($userData);
+
+        dd($payloadDataSub);
+    }
+}
--- config/services.php
+++ config/services.php
@@ -14,6 +14,25 @@ return [
     |
     */
 
+    'google' => [
+        'client_id' => '969738769103-5pr9h954os280esiepjgdnffvcbh200d.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://127.0.0.1:8000/social-google-callback'
+
+    ],
+
+    'linkedin' => [
+        'client_id' => '77rdbzbpmak1ze',
+        'client_secret' => 'KRkKrAEyNoLBp1dm',
+        'redirect' => 'http://127.0.0.1:8000/social-linkedin-callback',
+    ],
+
+    'github' => [
+        'client_id' => '3b3bdc001f61ba892ca6',
+        'client_secret' => '7d18a95f39a9579972e9f54add025c319a423c85',
+        'redirect' => 'http://127.0.0.1:8000/social-github-callback',
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
